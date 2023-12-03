896e3f8826fccaa43fbaa0bf6f13a0ef48f9169a
Thomas Twigden <ttwigden@deloitte.co.uk>
2023-12-03 15:22:14 +0000

    Implement day two logic, requiring a big refactor, tests passing

--- AoC.Core/Day1/CalibrationValueCalculator.cs
+++ AoC.Core/Day1/CalibrationValueCalculator.cs
@@ -4,36 +4,84 @@ namespace AoC.Core.Day1
 {
     public static class CalibrationValueCalculator
     {
+        private const string CalibrationNumberMatchingPattern = @"\d+";
+        private static readonly Dictionary<string, string> StringToNumConvertor = new()
+        {
+            {"one", "1"}, {"two", "2"}, {"three", "3"}, {"four", "4"}, {"five", "5"},
+            {"six", "6"}, {"seven", "7"}, {"eight", "8"}, {"nine", "9"}
+        };
+
         public static int CalculateTotal(List<string> calibrationValues)
         {
             int total = 0;
-            foreach (string calibrationValue in calibrationValues)
+
+            foreach (var calibrationValue in calibrationValues)
             {
-                total += Calculate(calibrationValue);
+                List<(string Word, int Index)> normalisedData = new();
+
+                AddNumberWordsToNormalisedData(calibrationValue, normalisedData);
+                AddNumbersToNormalisedData(calibrationValue, normalisedData);
+
+                var stringsFromNormalisedData = GetOrderedStringsFromNormalisedData(normalisedData);
+                var listOfNormalisedNumbers = ConvertWordsToNumbers(stringsFromNormalisedData);
+
+                if (!listOfNormalisedNumbers.Any())
+                {
+                    Console.WriteLine("No valid numbers found in the string, total will be 0.");
+                    return total;
+                }
+
+                var validTwoDigitNumber = CalculateCalibrationValue(listOfNormalisedNumbers);
+                total += validTwoDigitNumber;
             }
 
             return total;
         }
 
-        public static int Calculate(string calibrationValue)
+        private static int CalculateCalibrationValue(IEnumerable<string> listOfNormalisedNumbers)
         {
-            var digitMatchingPattern = @"\d+";
-            var matches = Regex.Matches(calibrationValue, digitMatchingPattern)
-                               .Cast<Match>()
-                               .Select(m => m.Value)
-                               .ToList();
-
-            if (!matches.Any())
-            {
-                throw new InvalidOperationException("No numbers found in the string.");
-            }
-
-            var firstDigit = matches.First().First();
-            var secondDigit = matches.Last().Last();
+            var firstDigit = listOfNormalisedNumbers.First().First();
+            var secondDigit = listOfNormalisedNumbers.Last().Last();
 
             var combinedString = $"{firstDigit}{secondDigit}";
-            var calculateCalibrationValue = int.Parse(combinedString);
-            return calculateCalibrationValue;
+            return int.Parse(combinedString);
+        }
+
+        private static IEnumerable<string> ConvertWordsToNumbers(IEnumerable<string> stringsFromNormalisedData)
+        {
+            return stringsFromNormalisedData
+                .Select(match => StringToNumConvertor.TryGetValue(match, out string? value) ? value : match);
+        }
+
+        private static IEnumerable<string> GetOrderedStringsFromNormalisedData(IEnumerable<(string Word, int Index)> normalisedData)
+        {
+            return normalisedData
+                    .OrderBy(valueTuple => valueTuple.Index)
+                    .Select(valueTuple => valueTuple.Word);
+        }
+
+        private static void AddNumbersToNormalisedData(string calibrationValue, List<(string Word, int Index)> normalisedData)
+        {
+            var numberMatches = Regex.Matches(calibrationValue, CalibrationNumberMatchingPattern)
+               .Cast<Match>();
+
+            foreach (var match in numberMatches)
+            {
+                normalisedData.Add((match.Value, match.Index));
+            }
+        }
+
+        private static void AddNumberWordsToNormalisedData(string calibrationValue, List<(string Word, int Index)> normalisedData)
+        {
+            foreach (var numString in StringToNumConvertor.Keys)
+            {
+                var index = calibrationValue.IndexOf(numString);
+                while (index != -1)
+                {
+                    normalisedData.Add((numString, index));
+                    index = calibrationValue.IndexOf(numString, index + 1);
+                }
+            }
         }
     }
 }
--- AoC.Test/Day1/CalibrationValueCalculatorTests.cs
+++ AoC.Test/Day1/CalibrationValueCalculatorTests.cs
@@ -8,36 +8,36 @@ namespace AoC.Test.Day1
         [Fact]
         public void Calculate_WhenGivenASingleDigitStringInput_ReturnsATwoDigitNumberOfTheSameDigit()
         {
-            var input = "1";
+            var input = new List<string> { "1" };
             var expectedOutput = 11;
-            var calibrationValue = CalibrationValueCalculator.Calculate(input);
+            var calibrationValue = CalibrationValueCalculator.CalculateTotal(input);
             calibrationValue.Should().Be(expectedOutput);
         }
 
         [Fact]
         public void Calculate_WhenGivenATwoDigitStringInput_ReturnsTheFirstAndLastDigitAsTwoDigitNumber()
         {
-            var input = "12";
+            var input = new List<string> { "12" };
             var expectedOutput = 12;
-            var calibrationValue = CalibrationValueCalculator.Calculate(input);
+            var calibrationValue = CalibrationValueCalculator.CalculateTotal(input);
             calibrationValue.Should().Be(expectedOutput);
         }
 
         [Fact]
         public void Calculate_WhenGivenASingleDigitStringInputWithLetters_ReturnsATwoDigitNumberOfTheSameDigit()
         {
-            var input = "1abc";
+            var input = new List<string> { "1abc" };
             var expectedOutput = 11;
-            var calibrationValue = CalibrationValueCalculator.Calculate(input);
+            var calibrationValue = CalibrationValueCalculator.CalculateTotal(input);
             calibrationValue.Should().Be(expectedOutput);
         }
 
         [Fact]
         public void Calculate_WhenGivenStringInputWithLettersAndNumbers_ReturnsTheFirstAndLastDigitAsTwoDigitNumber()
         {
-            var input = "abc123def456ghi789";
+            var input = new List<string> { "abc123def456ghi789" };
             var expectedOutput = 19;
-            var calibrationValue = CalibrationValueCalculator.Calculate(input);
+            var calibrationValue = CalibrationValueCalculator.CalculateTotal(input);
             calibrationValue.Should().Be(expectedOutput);
         }
 
@@ -46,6 +46,33 @@ namespace AoC.Test.Day1
         {
             var input = new List<string> { "abc123def456ghi789", "81345678", "abcndmd5jdskfusk" };
             var expectedOutput = 19 + 88 + 55;
+            var calibrationValue = CalibrationValueCalculator.CalculateTotal(input);
+            calibrationValue.Should().Be(expectedOutput);
+        }
+
+        [Fact]
+        public void Calculate_WhenGivenStringInputWithANumberWord_ReturnsATwoDigitNumberOfTheSameDigit()
+        {
+            var input = new List<string> { "one" };
+            var expectedOutput = 11;
+            var calibrationValue = CalibrationValueCalculator.CalculateTotal(input);
+            calibrationValue.Should().Be(expectedOutput);
+        }
+
+        [Fact]
+        public void CalculateTotal_WhenGivenStringInputsWithANumberWordAndNumbers_ReturnsTheTotalOfAllTwoDigitNumbersGenerated()
+        {
+            var input = new List<string> { "two1nine", "eightwothree", "abcone2threexyz", "xtwone3four", "4nineeightseven2", "zoneight234", "7pqrstsixteen" };
+            var expectedOutput = 29 + 83 + 13 + 24 + 42 + 14 + 76;
+            var calibrationValue = CalibrationValueCalculator.CalculateTotal(input);
+            calibrationValue.Should().Be(expectedOutput);
+        }
+
+        [Fact]
+        public void Calculate_WhenGivenStringInputsWithANumberWordThatOverlaps_ReturnsTheCorrectTwoDigitNumber()
+        {
+            var input = new List<string> { "twone", "eightwo", "sevenine", "1", "2two4", "101eightwo" };
+            var expectedOutput = 21 + 82 + 79 + 11 + 24 + 12;
             var calibrationValue = CalibrationValueCalculator.CalculateTotal(input);
             calibrationValue.Should().Be(expectedOutput);
         }
